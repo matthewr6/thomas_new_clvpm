1dc034873e43bf61e2bd8ed60380a7957f0e94d9
Matthew Radovan <mradovan@stanford.edu>
2023-08-09 21:43:02 -0700

    typo

--- thomas_v2_clvpm.sh
+++ thomas_v2_clvpm.sh
@@ -16,6 +16,6 @@ if [ ! -f "right/multiatlas_full.nii.gz" ]; then
     cd ../
 fi
 
-python3 $scriptpath/ombine_hemispheres.py left/multiatlas_full.nii.gz right/multiatlas_full.nii.gz multiatlas.nii.gz
+python3 $scriptpath/combine_hemispheres.py left/multiatlas_full.nii.gz right/multiatlas_full.nii.gz multiatlas.nii.gz
 python3 $scriptpath/thomas_prioritized_cl.py --use-existing-warps
 
